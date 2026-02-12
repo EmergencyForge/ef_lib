916b720118562d580cd3ab24378495d39051f34d
bitsystem <78836958+gitBitsystem@users.noreply.github.com>
2026-02-12 19:36:40 +0100

    Delete ef_lib directory
    fix structure

--- ef_lib/web/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-import './styles/main.css'
-import '@fortawesome/fontawesome-free/css/all.min.css'
-
-const app = createApp(App)
-app.use(createPinia())
-app.mount('#app')
